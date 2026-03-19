cb085f2a914a52e335730415d5bfd03d350a699f
Shubham Todkar <todkarshubham11@gmail.com>
2026-03-19 16:19:57 +0530

    Added Policy for user

--- eCommerce.Orderservice.BuisnessLogicLayer/HttpClients/UserMicroserviceClient.cs
+++ eCommerce.Orderservice.BuisnessLogicLayer/HttpClients/UserMicroserviceClient.cs
@@ -1,4 +1,6 @@
 ﻿using eCommerce.Orderservice.BuisnessLogicLayer.DTO;
+using Microsoft.Extensions.Logging;
+using Polly.CircuitBreaker;
 using System.Net.Http.Json;
 
 namespace eCommerce.Orderservice.BuisnessLogicLayer.HttpClients
@@ -6,28 +8,38 @@ namespace eCommerce.Orderservice.BuisnessLogicLayer.HttpClients
     public class UserMicroserviceClient
     {
         private readonly HttpClient _httpClient;
-        public UserMicroserviceClient(HttpClient httpClient)
+        private readonly ILogger<UserMicroserviceClient> _logger;
+        public UserMicroserviceClient(HttpClient httpClient, ILogger<UserMicroserviceClient> logger)
         {
             _httpClient = httpClient;
+            _logger = logger;
         }
 
         public async Task<UserDTO> GetUserByUserId(Guid userId)
         {
-            HttpResponseMessage response = await _httpClient.GetAsync($"/api/Users/{userId}");
-            if (!response.IsSuccessStatusCode)
+            try
             {
-                //throw new Exception("Failed to fetch user details");
-                return new UserDTO(new Guid("Temprory Unavailable"), "Temprory Unavailable", "Temprory Unavailable", "Temprory Unavailable");
-            }
-            else
-            {
-                UserDTO user = await response.Content.ReadFromJsonAsync<UserDTO>();
-                if(user == null)
+                HttpResponseMessage response = await _httpClient.GetAsync($"/api/Users/{userId}");
+                if (response.IsSuccessStatusCode)
                 {
-                    throw new Exception("User not found");
+                    UserDTO user = await response.Content.ReadFromJsonAsync<UserDTO>();
+                    if (user == null)
+                    {
+                        throw new Exception("User not found");
+                    }
+                    return user;
                 }
-                return user;
             }
+            catch(BrokenCircuitException ex)
+            {
+                _logger.LogInformation(ex, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogInformation(ex,ex.Message);
+            }
+
+            return new UserDTO(Guid.Empty, "Temprory Unavailable", "Temprory Unavailable", "Temprory Unavailable");
         }
     }
 }
